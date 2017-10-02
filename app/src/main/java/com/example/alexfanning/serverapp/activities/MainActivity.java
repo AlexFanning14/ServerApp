445b3f3cfbb3efcb66604faa53cbc1e289bdfad7
alex.fanning <alexfanning14@gmail.com>
2017-10-02 16:53:29 +0100

    Minor Changes

--- app/src/main/java/com/example/alexfanning/serverapp/activities/MainActivity.java
+++ app/src/main/java/com/example/alexfanning/serverapp/activities/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.alexfanning.serverapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.alexfanning.serverapp.R;
+import com.example.alexfanning.serverapp.fileitems.File;
+import com.example.alexfanning.serverapp.fileitems.FileDataAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findViews();
+        mRv.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false));
+        mRv.setHasFixedSize(true);
+        File[] fs = new File[]{new File(1,"test","testPath"), new File(2,"secodn test", "Test")};
+        FileDataAdapter fda = new FileDataAdapter(fs,this);
+        mRv.setAdapter(fda);
     }
 
     private void findViews(){
